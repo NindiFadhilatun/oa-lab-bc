7d721d481b004a73d48021d5f923140690d81e65
okiprakasa <okiprakasa@gmail.com>
2022-05-09 17:45:44 +0800

    Clean Code quote-scraper.go and add min height to login.css

--- internal/scraper/quote-scraper.go
+++ internal/scraper/quote-scraper.go
@@ -9,7 +9,7 @@ import (
 )
 
 //goland:noinspection SpellCheckingInspection
-var quotesHardcoded = []string{"Jangan Pernah Lelah Mencintai Negeri Ini.",
+var localQuotes = []string{"Jangan Pernah Lelah Mencintai Negeri Ini.",
 	"Kurang cerdas bisa diperbaiki dengan belajar. Kurang cakap dapat dihilangkan dengan pengalaman. Namun tidak jujur itu sulit diperbaiki.",
 	"Setiap orang menjadi guru, setiap rumah menjadi sekolah.",
 	"Bermimpilah setinggi langit. Jika kamu jatuh, kamu akan jatuh di antara bintang-bintang.",
@@ -23,10 +23,10 @@ var quotesHardcoded = []string{"Jangan Pernah Lelah Mencintai Negeri Ini.",
 	"Indonesia merdeka bukan tujuan akhir kita. Indonesia merdeka hanya syarat untuk bisa mencapai kebahagiaan dan kemakmuran rakyat.",
 	"Kalau sistem itu tak bisa diperiksa kebenarannya dan tak bisa dikritik, maka matilah ilmu pasti itu.",
 	"Jangan pernah berhenti dan jangan pernah lelah untuk memberikan yang terbaik dan melakukan inovasi serta kreativitas untuk mendukung tugas-tugas DJBC."}
-var authorsHardcoded = []string{"Sri Mulyani", "Mohammad Hatta", "Ki Hadjar Dewantara", "Soekarno", "Bung Tomo", "Soekarno", "Mohammad Hatta", "Soekarno", "Soedirman", "R.A. Kartini", "Soedirman", "Mohammad Hatta", "Tan Malaka", "Askolani"}
+var localAuthors = []string{"Sri Mulyani", "Mohammad Hatta", "Ki Hadjar Dewantara", "Soekarno", "Bung Tomo", "Soekarno", "Mohammad Hatta", "Soekarno", "Soedirman", "R.A. Kartini", "Soedirman", "Mohammad Hatta", "Tan Malaka", "Askolani"}
 var uniqueTimeNow = rand.NewSource(time.Now().UnixNano())
 
-// ScrapeQuoteWithTimeLimiter limit how long QuoteScraper can run, otherwise return default value Bea Cukai Makin Baik
+// ScrapeQuoteWithTimeLimiter limit how long QuoteScraper can run, otherwise return random locally hardcoded quote and author
 func ScrapeQuoteWithTimeLimiter(timeLimitInMs float64) (quote string, author string) {
 	timeLimit := time.Duration(timeLimitInMs) * time.Millisecond
 	done := make(chan struct{})
@@ -36,46 +36,45 @@ func ScrapeQuoteWithTimeLimiter(timeLimitInMs float64) (quote string, author str
 	}()
 	select {
 	case <-done:
-	case <-time.After(timeLimit): //Not finished scraping before timeLimitInMs
-		quote, author = getHardcodedQuoteAndAuthor()
+	case <-time.After(timeLimit): //Case in which system has not yet finished scraping before timeLimit ends
+		quote, author = getLocalQuoteAndAuthor()
 	}
 	return quote, author
 }
 
-//QuoteScraper scrape quotes from quotes.toscrape.com on random page (1-10) and random n-th of quote found on the page
+//QuoteScraper scrape quotes from quotes.toscrape.com on random page & random n-th of quote found on the page
 func QuoteScraper() (string, string) {
 	c := colly.NewCollector()
-	var webQuotes, webAuthors []string
+	var onlineQuotes, onlineAuthors []string
 	c.OnHTML(".quote", func(e *colly.HTMLElement) {
-		webQuotes = append(webQuotes, e.ChildText(".text"))
-		webAuthors = append(webAuthors, e.ChildText(".author"))
+		onlineQuotes = append(onlineQuotes, e.ChildText(".text"))
+		onlineAuthors = append(onlineAuthors, e.ChildText(".author"))
 	})
 	err := c.Visit(getRandomPage())
 	if err != nil {
-		return getHardcodedQuoteAndAuthor()
+		return getLocalQuoteAndAuthor()
 	}
-	if len(webQuotes) > 0 && len(webQuotes) == len(webAuthors) { //Total webQuotes must be the same with total of webAuthors (pairing to each other)
-		return getWebQuoteAndAuthor(webQuotes, webAuthors)
+	if len(onlineQuotes) > 0 && len(onlineQuotes) == len(onlineAuthors) { //Each quote & author must pair to each other)
+		quoteRandom := rand.New(uniqueTimeNow).Intn(len(onlineQuotes) - 1)
+		return formatScrapedQuote(onlineQuotes[quoteRandom]), onlineAuthors[quoteRandom]
 	} else {
-		return getHardcodedQuoteAndAuthor()
+		return getLocalQuoteAndAuthor()
 	}
 }
 
-func getHardcodedQuoteAndAuthor() (quote string, author string) {
-	randomForHardcoded := rand.New(uniqueTimeNow).Intn(len(authorsHardcoded) - 1)
-	return quotesHardcoded[randomForHardcoded], authorsHardcoded[randomForHardcoded]
+//getLocalQuoteAndAuthor get locally hardcoded quote and author
+func getLocalQuoteAndAuthor() (quote string, author string) {
+	randomForLocal := rand.New(uniqueTimeNow).Intn(len(localAuthors) - 1)
+	return localQuotes[randomForLocal], localAuthors[randomForLocal]
 }
 
+//getRandomPage get random page to scrape from quotes.toscrape.com
 func getRandomPage() (page string) {
-	pageRandom := rand.New(uniqueTimeNow).Intn(9) + 1 //will result of random number from 1 to 10
+	pageRandom := rand.New(uniqueTimeNow).Intn(9) + 1 //Random number from 1 to 10
 	return "https://quotes.toscrape.com/page/" + strconv.Itoa(pageRandom)
 }
 
-func getWebQuoteAndAuthor(quotes, authors []string) (quote string, author string) {
-	quoteRandom := rand.New(uniqueTimeNow).Intn(len(quotes) - 1)
-	return formatScrapedQuote(quotes[quoteRandom]), authors[quoteRandom]
-}
-
+//formatScrapedQuote format quote scraped from web with the right formatting
 func formatScrapedQuote(quote string) (formattedQuote string) {
 	return strings.ReplaceAll(strings.ReplaceAll(quote, "“", ""), "”", "")
 }
